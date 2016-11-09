da002827b0be6c937992541b8d5ce819147d7ae7
shlyren <shlyren@163.com>
2016-11-09 17:27:20 +0800

    缧

--- YXCategories/YXCategories.h
+++ YXCategories/YXCategories.h
@@ -6,24 +6,6 @@
 //  Copyright © 2016年 任玉祥. All rights reserved.
 //
 
-#if __has_include(<YXCategories/YXCategories.h>)
-
-#import <YXCategories/NSBundle+Extension.h>
-#import <YXCategories/UICollectionView+Extension.h>
-#import <YXCategories/UITableView+Extension.h>
-#import <YXCategories/UIBarButtonItem+Extension.h>
-#import <YXCategories/NSString+Extension.h>
-#import <YXCategories/NSUserDefaults+Extension.h>
-#import <YXCategories/UIButton+Extension.h>
-#import <YXCategories/UIColor+Extension.h>
-#import <YXCategories/UIImage+Extension.h>
-#import <YXCategories/UIScrollView+Extension.h>
-#import <YXCategories/UIView+Extension.h>
-#import <YXCategories/UIView+Frame.h>
-#import <YXCategories/UIViewController+Extension.h>
-
-#else
-
 #import "NSBundle+Extension.h"
 #import "UICollectionView+Extension.h"
 #import "UITableView+Extension.h"
@@ -37,7 +19,7 @@
 #import "UIView+Extension.h"
 #import "UIView+Frame.h"
 #import "UIViewController+Extension.h"
-#endif
+
 
 
 
