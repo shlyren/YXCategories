9b5e017e65e354ae0fd4378fb980c058fdb17f68
shlyren <shlyren@163.com>
2016-11-11 18:11:25 +0800

    1.4.1

--- YXCategories/Foundation/NSString+Extension.m
+++ YXCategories/Foundation/NSString+Extension.m
@@ -8,7 +8,6 @@
 
 #import "NSString+Extension.h"
 #import <CommonCrypto/CommonDigest.h>
-#import "UIColor+Extension.h"
 
 @implementation NSString (Extension)
 /**
@@ -146,7 +145,50 @@
 
 - (UIColor *)color
 {
-    return [UIColor colorWithHexString:self];
+    //删除字符串中的空格
+    NSString *cString = [[self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    // String should be 6 or 8 characters
+    if ([cString length] < 6)
+    {
+        return [UIColor clearColor];
+    }
+    // strip 0X if it appears
+    //如果是0x开头的，那么截取字符串，字符串从索引为2的位置开始，一直到末尾
+    if ([cString hasPrefix:@"0X"])
+    {
+        cString = [cString substringFromIndex:2];
+    }
+    //如果是#开头的，那么截取字符串，字符串从索引为1的位置开始，一直到末尾
+    if ([cString hasPrefix:@"#"])
+    {
+        cString = [cString substringFromIndex:1];
+    }
+    if ([cString length] != 6)
+    {
+        return [UIColor clearColor];
+    }
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    //r
+    NSString *rString = [cString substringWithRange:range];
+    //g
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    //b
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((CGFloat)r / 255.0f) green:((CGFloat)g / 255.0f) blue:((CGFloat)b / 255.0f) alpha:1.0f];
+
 }
 
 - (NSArray *)arryWithEndDate:(NSString *)endDateStr;
